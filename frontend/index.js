20b7fabf1c7abd89ce57cb3075c1edb5a8e1752c
joey468 <yossi.heifetz06@gmail.com>
2025-06-01 14:53:29 +0300

    update call for spin

--- frontend/index.js
+++ frontend/index.js
@@ -97,7 +97,8 @@ const spin = async () => {
     const response = await fetch(`${BASE_URL}/spin`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ lines, bet, balance })
+      credentials: "include", 
+      body: JSON.stringify({ lines, bet }) 
     });
 
     const data = await response.json();
